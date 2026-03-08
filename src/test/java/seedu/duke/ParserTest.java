6c4c2b56121f3f2fd449eb2e6aaedfce4fd888f9
Chua Yong Liang <e0702014@u.nus.edu>
2026-03-08 11:23:28 +0800

    Parser class: Add unit tests

--- src/test/java/seedu/duke/ParserTest.java
+++ src/test/java/seedu/duke/ParserTest.java
@@ -2,6 +2,7 @@ package seedu.duke;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 
@@ -15,5 +16,25 @@ public class ParserTest {
         assertEquals("Pending", result.getStatus());
         assertNull(result.getDeadline());
         assertNull(result.getContact());
+    }
+
+    @Test
+    public void parse_addCommandWithExtraSpaces_success () throws InternTrackException {
+        String input = "add c/ Google r/ Software Engineer";
+        Application result = Parser.createApplication(input);
+        assertEquals("Google", result.getCompany());
+        assertEquals("Software Engineer", result.getRole());
+        assertEquals("Pending", result.getStatus());
+        assertNull(result.getDeadline());
+        assertNull(result.getContact());
+    }
+
+    @Test
+    public void parse_missingCompanyPrefix_throwsInternTrackException() {
+        String input = "add r/Software Engineer";
+        InternTrackException exception = assertThrows(InternTrackException.class, () -> {
+            Parser.createApplication(input);
+        });
+        assertEquals("Both company (c/) and role (r/) are required!", exception.getMessage());
     }
 }
